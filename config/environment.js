0ac2df12bda25be0a927e50a28e834700fca0d8e
Marveeeen <jmvroqueee@gmail.com>
2026-02-21 22:13:40 +0800

    feat: implement map component

--- config/environment.js
+++ config/environment.js
@@ -44,5 +44,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.TOMTOM_ACCESS_TOKEN = 'Xst6elj9QIdCVRNmJSxZuJfKkFMUK2bB';
+
   return ENV;
 };
